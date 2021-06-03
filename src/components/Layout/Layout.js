3695b5b6cdca827af3606969f6340925c277c525
Iden Han <idenhan129@gmail.com>
2021-06-03 16:44:38 +0900

    Fix errors on getStaticPaths

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import Img from "next/image";
 import styles from "./Layout.module.css";
 import Logo from "../../images/logo.png";
@@ -11,13 +12,15 @@ const Layout = ({ children, title = "NURI" }) => {
         <link rel="icon" href="/favicon.png" />
       </Head>
       <header className={styles.header}>
-        <Img
-          style={{ top: 0 }}
-          src={Logo}
-          alt="nuri-logo"
-          width={175}
-          height={130}
-        />
+        <Link href="/">
+          <Img
+            style={{ top: 0 }}
+            src={Logo}
+            alt="nuri-logo"
+            width={175}
+            height={130}
+          />
+        </Link>
       </header>
       <main className={styles.main}>{children}</main>
       <footer className={styles.footer}>Iden Han @ idenhan.com</footer>
