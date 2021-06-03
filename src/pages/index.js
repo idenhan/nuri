c479537a817a682a2fcbe45453139e95bfffa671
Iden Han <idenhan129@gmail.com>
2021-06-03 14:41:32 +0900

    Implement SearchInput

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import Layout from "../components/Layout/Layout";
+import SearchInput from "../components/SearchInput/SearchInput";
 import styles from "../styles/Home.module.css";
 
 export default function Home({ countries }) {
-  console.log(countries);
-  return <Layout>main</Layout>;
+  return (
+    <Layout>
+      <div className={styles.counts}>Found {countries.length} countries</div>
+      <SearchInput placeholder="Filter by Name, Region or SubRegion" />
+    </Layout>
+  );
 }
 
 export const getStaticProps = async () => {
