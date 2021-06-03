9818f227c5604cf19e93babee6905b00aaa6e6f0
Iden Han <idenhan129@gmail.com>
2021-06-03 14:18:06 +0900

    Enable to load png files

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import Img from "next/image";
+import styles from "./Layout.module.css";
+import Logo from "../../images/logo.png";
+
+const Layout = ({ children, title = "NURI" }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <header className={styles.header}>
+        <Img
+          style={{ top: 0 }}
+          src={Logo}
+          alt="nuri-logo"
+          width={175}
+          height={130}
+        />
+      </header>
+      <main className={styles.main}>{children}</main>
+      <footer className={styles.footer}>Iden Han @ idenhan.com</footer>
+    </div>
+  );
+};
+
+export default Layout;
